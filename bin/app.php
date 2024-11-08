8b37352efc4cae2f3f7dce33c4a98cf8088a247b
jnkmtsd <admin@gleampine.com>
2024-11-09 00:23:22 +0900

    FREELANCE-248: バグ修正

--- bin/app.php
+++ bin/app.php
@@ -6,6 +6,6 @@ require __DIR__ . '/../vendor/autoload.php';
 $c = require __DIR__ . '/../bootstrap/container.php';
 
 $apiClientA = $c->make(\Qps\Feature\DesignPattern\Decorator\ApiClientA::class);
-$apiClientB = $c->make(\Qps\Feature\DesignPattern\Decorator\ApiClientA::class);
+$apiClientB = $c->make(\Qps\Feature\DesignPattern\Decorator\ApiClientB::class);
 var_dump($apiClientA->request());
 var_dump($apiClientB->request());
